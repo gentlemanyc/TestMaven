1a04c7f1cc029fae0fc9014525170bd895d64532
15655576176@163.com <yc2596361>
2019-06-13 14:27:18 +0800

    提交1

--- src/main/java/com/example/mylib/MyLibUtil.java
+++ src/main/java/com/example/mylib/MyLibUtil.java
@@ -4,4 +4,8 @@ public class MyLibUtil {
     public static void fuckYou() {
         System.out.println("fuckyou.");
     }
+
+    public static void hello() {
+        System.out.println("hello");
+    }
 }
